e7eeba902e39ba356591a84e5a3362086ef6b404
yanxuanshaozhu <973493269@qq.com>
2021-03-23 23:01:37 +0800

    project0: all finished

---
+++ proj0/NBody.java
@@ -0,0 +1,80 @@
+public class NBody {
+
+    /**
+     * Read and returns the radius of a universe contained in a given file
+     * 
+     * @param filename the name of the give file
+     * @return returns the double radius of the universe
+     */
+    public static double readRadius(String filename) {
+        In in = new In(filename);
+        int amount = in.readInt();
+        double radius = in.readDouble();
+        return radius;
+    }
+
+    /**
+     * Read and return a list of planets from a give file;
+     * 
+     * @param filename the name of the give file
+     * @return returns a list of planets contained in the file
+     */
+    public static Planet[] readPlanets(String filename) {
+        In in = new In(filename);
+        int amount = in.readInt();
+        double radius = in.readDouble();
+        Planet[] planets = new Planet[amount];
+        int index = 0;
+        while (index < amount) {
+            double xP = in.readDouble();
+            double yP = in.readDouble();
+            double xV = in.readDouble();
+            double yV = in.readDouble();
+            double m = in.readDouble();
+            String img = in.readString();
+            planets[index] = new Planet(xP, yP, xV, yV, m, img);
+            index += 1;
+        }
+        return planets;
+    }
+
+    public static void main(String[] args) {
+        double T = Double.valueOf(args[0]);
+        double dt = Double.valueOf(args[1]);
+        String filename = args[2];
+        double radius = readRadius(filename);
+        Planet[] planets = readPlanets(filename);
+
+        StdDraw.setScale(-radius, radius);
+        StdDraw.picture(0, 0, "./images/starfield.jpg");
+        for (Planet planet : planets) {
+            planet.draw();
+        }
+
+        StdDraw.enableDoubleBuffering();
+
+        double time = 0;
+        int amount = planets.length;
+        while (time <= T) {
+            double[] xForces = new double[amount];
+            double[] yForces = new double[amount];
+            for (int i = 0; i < amount; i++) {
+                Planet planet = planets[i];
+                xForces[i] = planet.calcNetForceExertedByX(planets);
+                yForces[i] = planet.calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < amount; i++) {
+                Planet planet = planets[i];
+                planet.update(time, xForces[i], yForces[i]);
+            }
+            StdDraw.picture(0, 0, "./images/starfield.jpg");
+            for (Planet planet : planets) {
+                planet.draw();
+            }
+
+            StdDraw.show();
+            StdDraw.pause(10);
+            time += dt;
+        }
+    }
+}
---
+++ proj0/Planet.java
@@ -0,0 +1,167 @@
+public class Planet {
+
+    public static double gravityConst = 6.67e-11;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    /**
+     * The first constructor, creates a planet with given parameters
+     * 
+     * @param xP: planet x position
+     * @param yP: planet y position
+     * @param xV: planet x direction velocity
+     * @param yV: planet y direction velocity
+     * @m: planet mass
+     * @img: planet figure
+     */
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
+    }
+
+    /**
+     * Second constructor, carets a planet that is the same as the given planet.
+     * 
+     * @param a given planet
+     * 
+     */
+    public Planet(Planet p) {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    /**
+     * Calculate the distance between self planet and a given planet
+     * 
+     * @param p a given planet
+     * @return returns the distance between self planet and a given planet
+     */
+    public double calcDistance(Planet p) {
+        double dx = p.xxPos - this.xxPos;
+        double dy = p.yyPos - this.yyPos;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    /**
+     * Calculate the force between self planet and a given planet
+     * 
+     * @param p a given planet
+     * @return returns the force between self planet and a given planet
+     */
+    public double calcForceExertedBy(Planet p) {
+        double dist = this.calcDistance(p);
+        return Planet.gravityConst * this.mass * p.mass / (dist * dist);
+    }
+
+    /**
+     * Calculate the force in x direction between self planet and a given planet
+     * 
+     * @param p a given planet
+     * @return returns the force in x direction between self planet and a given
+     *         planet
+     */
+    public double calcForceExertedByX(Planet p) {
+        double dx = p.xxPos - this.xxPos;
+        double dist = this.calcDistance(p);
+        double force = this.calcForceExertedBy(p);
+        return force * dx / dist;
+    }
+
+    /**
+     * Calculate the force in y direction between self planet and a given planet
+     * 
+     * @param p a given planet
+     * @return returns the force in y direction between self planet and a given
+     *         planet
+     */
+    public double calcForceExertedByY(Planet p) {
+        double dy = p.yyPos - this.yyPos;
+        double dist = this.calcDistance(p);
+        double force = this.calcForceExertedBy(p);
+        return force * dy / dist;
+    }
+
+    /**
+     * Calculate the net force in x direction among self planet and a list of
+     * planets
+     * 
+     * @param planets a list of planets
+     * @return returns the net force in x direction among self planet and a list of
+     *         planets
+     */
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double netforcex = 0;
+        for (Planet planet : planets) {
+            if (!this.equals(planet)) {
+                double dx = planet.xxPos - this.xxPos;
+                double dist = this.calcDistance(planet);
+                double force = this.calcForceExertedBy(planet);
+                double forcex = force * dx / dist;
+                netforcex += forcex;
+            }
+        }
+        return netforcex;
+    }
+
+    /**
+     * Calculate the net force in y direction among self planet and a list of
+     * planets
+     * 
+     * @param planets a list of planets
+     * @return returns the net force in y direction among self planet and a list of
+     *         planets
+     */
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double netforcey = 0;
+        for (Planet planet : planets) {
+            if (!this.equals(planet)) {
+                double dy = planet.yyPos - this.yyPos;
+                double dist = this.calcDistance(planet);
+                double force = this.calcForceExertedBy(planet);
+                double forcey = force * dy / dist;
+                netforcey += forcey;
+            }
+        }
+        return netforcey;
+    }
+
+    /**
+     * Update self velocity and self position
+     * 
+     * @param time   the duration of the force
+     * @param forcex force in x direction
+     * @param forcey force in y direction
+     */
+    public void update(double time, double forcex, double forcey) {
+        double ax = forcex / this.mass;
+        double ay = forcey / this.mass;
+        this.xxVel += time * ax;
+        this.yyVel += time * ay;
+        this.xxPos += this.xxVel * time;
+        this.yyPos += this.yyVel * time;
+    }
+
+    /**
+     * Draw a picture of the planet using the StdDraw library
+     */
+    public void draw() {
+        
+        double x = this.xxPos;
+        double y = this.yyPos;
+        String filename = this.imgFileName;
+        StdDraw.picture(x, y, filename);
+    }
+
+}
